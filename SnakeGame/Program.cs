0f6eb759c45a54cddbe51ce9e9c0c0d5e35d2e25
xfje <93822528+xfje@users.noreply.github.com>
2023-11-29 11:13:08 +0100

    Update Program.cs
    Zmienione:
     if (hoofd.xPos == obstacleXpos && hoofd.yPos == obstacleYpos)

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -199,7 +199,7 @@ class Program
 
             //Hindernis treffen
 
-            if (hoofd.xPos == obstacleXpos /* ?? */ == obstacleYpos)
+            if (hoofd.xPos == obstacleXpos && hoofd.yPos == obstacleYpos)
 
             {
 
